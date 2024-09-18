a70e50bffaee639f471458c5346175da31c07aa2
paramjeetn <paramjeenpradhan@gmail.com>
2024-09-18 09:22:35 +0530

    deleted chatbot

--- src/app/(webpages)/admin/page.tsx
+++ src/app/(webpages)/admin/page.tsx
@@ -1,6 +1,5 @@
 "use client"
 import { useState } from 'react'
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
